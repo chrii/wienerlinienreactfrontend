8e98784e00cf69c277c1a591d36fe75b6df79cd4
chris <c.pintarich@gmail.com>
2020-02-26 17:38:49 +0100

    First working commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { ThemeProvider } from "@material/core/ThemeProvider";
-import { createMuiTheme } from "@material/core/styles";
-import lightBlue from "@material/core/colors/lightBlue";
-import teal from "@material/core/colors/teal";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import lightBlue from "@material-ui/core/colors/lightBlue";
+import teal from "@material-ui/core/colors/teal";
+import Grid from "@material-ui/core/Grid";
+import Layout from "./components/LandingPage/Layout";
 
 class App extends Component {
   theme = createMuiTheme({
@@ -16,7 +18,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ThemeProvider theme={this.theme}></ThemeProvider>
+        <ThemeProvider theme={this.theme}>
+          <Layout />
+          <Grid container spacing={3}>
+            <Grid item xs></Grid>
+            <Grid item xs spacing={6}></Grid>
+            <Grid item xs></Grid>
+          </Grid>
+        </ThemeProvider>
       </div>
     );
   }
